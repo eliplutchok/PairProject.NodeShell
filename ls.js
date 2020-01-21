764f5f001fb25459bb4a7993bdd70793604a9e44
eli <eliplutchok1@gmail.com>
2020-01-21 12:59:12 -0500

    Completed pwd and ls

--- ls.js
+++ ls.js
@@ -1,17 +1,26 @@
 module.exports = function () {
     process.stdout.write('prompt > ');
 
-    fs.readdir('./', 'utf8', (err, files) => {
-        if (err) {
-            throw err
-        } else {
-            process.stdout.write(files.join('\n'))
-            process.stdout.write("prompt > ")
-        }
-    })
 
-  }
+    process.stdin.on('data', (data) => {
+
+        const cmd = data.toString().trim();
+             
+            if (cmd === 'ls'){
+                 const fs = require('fs');
+                   fs.readdir('./', 'utf8', (err, files) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        process.stdout.write(files.join('\n'))
+                        process.stdout.write("prompt > ")
+                    }
+                    } )
+            }
+      
+       }  )
+}
   
   
   
-  
+ 
